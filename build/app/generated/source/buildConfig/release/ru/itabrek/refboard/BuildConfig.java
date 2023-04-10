bb6e9bd3b5596165b4b93d60d9cfe62027d92b00
rozoomcool <rozoom.um@gmail.com>
2023-04-10 09:18:27 +0300

    Restore lib folder

---
+++ build/app/generated/source/buildConfig/release/ru/itabrek/refboard/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ru.itabrek.refboard;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "ru.itabrek.refboard";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
